65d5e1ecdf6bd62c5d2e7a6a762ab48cf00aed73
nljacobson <nljacobs@uvm.edu>
2023-03-08 01:50:55 -0500

    Created the authentication method, and redirect to a main page yet to be built.

---
+++ backend/authentication/serializers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import authenticate
+
+from rest_framework import serializers
+
+class LoginSerializer(serializers.Serializer):
+    """
+    This serializer defines two fields for authentication:
+      * username
+      * password.
+    It will try to authenticate the user with when validated.
+    """
+    username = serializers.CharField(
+        label="Username",
+        write_only=True
+    )
+    password = serializers.CharField(
+        label="Password",
+        # This will be used when the DRF browsable API is enabled
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+
+    def validate(self, attrs):
+        # Take username and password from request
+        username = attrs.get('username')
+        password = attrs.get('password')
+        if username and password:
+            # Try to authenticate the user
+            user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+            if not user:
+                # Case of no user found
+                msg = 'Access denied: wrong username or password.'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = 'Both "username" and "password" are required.'
+            raise serializers.ValidationError(msg, code='authorization')
+        attrs['user'] = user
+        
+        return attrs
